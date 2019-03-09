4d0c0da262706881c55a6b6f549d507e6530094d
Roman <r.poliakh@gmail.com>
2019-03-09 17:03:53 +0200

    - added layouts biseline and surfase;

--- myportfolio/script/script.js
+++ myportfolio/script/script.js
@@ -1,1 +1,25 @@
-"use strict";console.log("Hello World");
+let mylist = document.createDocumentFragment();
+let parentCards = document.querySelector('.cardWrap');
+let elemCard = parentCards.querySelector(".card");
+parentCards.removeChild(elemCard);
+elemCard.hidden = false;
+
+let createCard = (obj) => {
+	let newCard = elemCard.cloneNode(true);
+	newCard.querySelector('.card__page').href = obj.gitpage;
+	newCard.querySelector('.card__hub').href = obj.github;
+	newCard.style.backgroundImage = `url("${obj.preview}")`;
+	return newCard;
+}
+let parsObj = obj =>{
+	for (let key in obj){
+		mylist.appendChild(createCard(obj[key]));
+	}
+	parentCards.appendChild(mylist);	
+}
+
+fetch('ololo.json')
+.then(response => response.json())
+.then(parsObj)
+.catch( alert );
+
