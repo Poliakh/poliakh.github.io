62fe75ce96efa42d96a3b7194fe78894f33cd112
Roman <r.poliakh@gmail.com>
2020-01-05 23:51:38 +0200

    - edit for portfolio  - edit for test of itcreative

--- myportfolio/site/itcreative_test/script/script.js
+++ myportfolio/site/itcreative_test/script/script.js
@@ -1,6 +1,2 @@
-console.log("Hello World");
-
-{
-
-}
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJzY3JpcHQuanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coXCJIZWxsbyBXb3JsZFwiKTtcclxuXHJcbntcclxuXHJcbn1cclxuIl0sImZpbGUiOiJzY3JpcHQuanMifQ==
+console.log("Hello World")
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJzY3JpcHQuanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coXCJIZWxsbyBXb3JsZFwiKVxyXG5cclxuIl0sImZpbGUiOiJzY3JpcHQuanMifQ==
---
+++ myportfolio/site/itcreative_test/script/script_IE.js
@@ -0,0 +1,2 @@
+console.log('this IE');
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJzY3JpcHRfSUUuanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coJ3RoaXMgSUUnKTtcclxuIl0sImZpbGUiOiJzY3JpcHRfSUUuanMifQ==
