a6687e62d4064e9dc5365af2ae2dd6788c146b1f
Roman <r.poliakh@gmail.com>
2020-02-08 21:29:24 +0200

    - added popups for site novamed

--- myportfolio/site/nvmed/script/script.js
+++ myportfolio/site/nvmed/script/script.js
@@ -15,14 +15,12 @@ class Popup {
 	popUpShow(target) {
 		this.popup.classList.remove("popupHidden");
 		const body = document.querySelector('body');
-
-		body.classList.add('modal-open')
+		body.classList.add('modal-open');
 	}
 	popUpHide() {
 		this.popup.classList.add("popupHidden");
 		const body = document.querySelector('body');
 		body.classList.remove('modal-open')
-
 	}
 	closeByKey(event) {
 		if (event.keyCode === 27) {
@@ -39,8 +37,10 @@ class Popup {
 	}
 }
 class PersonService {
+	
 	constructor(url) {
 		this.url = url;
+		this.getResource();
 	}
 	async getResource() {
 		const res = await fetch(this.url);
@@ -54,6 +54,12 @@ class PersonService {
 		const res = await this.getResource();
 		return [res.persons, res.urlImages];
 	}
+	async getListFullName() {
+		const [list,] = await this.getPersonList();
+		return list.map(item => {
+			return `${item.surename} ${item.name} ${item.patronymic}`
+		})
+	}
 
 	async getPerson(key) {
 		const [list, url] = await this.getPersonList();
@@ -64,6 +70,7 @@ class PersonService {
 function createCard(obj, urlImage) {
 	const CardPerson = document.getElementById("CardsDoc");
 	const newCard = CardPerson.cloneNode(true);
+	// console.log(obj);
 
 	newCard.removeAttribute("hidden");
 	newCard.id = obj.id;
@@ -110,9 +117,129 @@ async function createPopup(target) {
 
 class doctorPopup extends Popup {
 	popUpShow(target) {
-		createPopup(target)
 		super.popUpShow(target);
+		createPopup(target)
 	}
 
 }
-const myPopup = new doctorPopup("popupWrap", ".cardPerson", ".closeButton");
+const myDoctor = new doctorPopup("doctorPopup", ".cardPerson", ".closeButton", "popupDoc");
+// document.addEventListener('click', (e) => {
+// 	// console.log(e.target);
+// })
+
+
+let myDatePicker = $('.datepicker-here').datepicker({
+	// autoClose: true,
+	classes: "my-datepicker",
+	inline: true,
+	language: {
+		days: ['Неділя', 'Понеділок', 'Вівторок', 'Середа', 'Четвер', "П'ятница", 'Субота'],
+		daysShort: ['Нед', 'Пон', 'Вів', 'Сер', 'Чет', "П'ят", 'Суб'],
+		daysMin: ['Нд', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+		months: ['Січень', 'Лютій', 'Березнь', 'Квітень', 'Травень', 'Червень', 'Липень', 'Серпень', 'Вересень', 'Жовтень', 'Листопад', 'Грудень'],
+		monthsShort: ['Січ', 'Лют', 'Бер', 'Кві', 'Тра', 'Чер', 'Лип', 'Сер', 'Вер', 'Жов', 'Лис', 'Гру'],
+		today: 'Сьогодні',
+		clear: 'Очистити',
+		dateFormat: 'dd.mm.yyyy',
+		timeFormat: 'hh:ii',
+		firstDay: 1
+
+	},
+	clearButton: true,
+	todayButton: true,
+	todayButton: new Date(),
+	disabledDays: [0, 6],
+	onRenderCell: function (date, cellType) {
+		if (cellType == 'day') {
+			var day = date.getDay(),
+				isDisabled = this.disabledDays.indexOf(day) != -1;
+			return {
+				disabled: isDisabled
+			}
+		}
+	}
+})
+
+
+jQuery(function($){
+	// $("#date").mask("99/99/9999");
+	$("#user_phone").mask("+38(999) 999-99-99");
+	// $("#tin").mask("99-9999999");
+	// $("#ssn").mask("999-99-9999");
+ });
+
+
+const selectExpert = (popupId, selectArea, selectItem) => {
+	const popupForm = document.getElementById(popupId);
+	let selectHead = document.querySelector(`.${selectArea}`);
+
+	async function createSelectItem() {
+		const item = popupForm.querySelector(`.${selectItem}`);
+		const parent = item.parentElement;
+		const cloneItem = item.cloneNode();
+		item.remove();
+		const list = await persons.getListFullName();
+		for (let i = 0; i < list.length; i++) {
+			const el = cloneItem.cloneNode();
+			el.innerHTML = list[i];
+			parent.appendChild(el);
+		}
+	}
+
+	const selectItems = (e) => {
+		let target = e.target;
+		if (target.closest(`.${selectArea}`)) {
+			selectHead.classList.add(`${selectArea}-active`)
+		}
+		if (target.closest(`.${selectItem}`)) {
+			let list = selectHead.querySelectorAll(`.${selectItem}`);
+			for (let i = 0; i < list.length; i++) {
+				if (target == list[i]) {
+					let select = selectHead.querySelector('select');
+					let option = select.querySelector('option');
+					option.innerHTML = list[i].innerHTML;
+					option.selected = true;
+					selectHead.firstElementChild.innerHTML = list[i].innerHTML;
+				}
+			}
+			selectHead.classList.remove(`${selectArea}-active`);
+			selectHead.blur();
+			selectHead.parentElement.nextElementSibling.focus();
+		}
+	}
+	createSelectItem();
+	popupForm.addEventListener('click', selectItems)
+
+
+}
+selectExpert('popupForm', 'select__header', 'select__item');
+
+
+const selectDate = (popupId, input, activeEl, autoCloseClassName)=>{
+		const popup = document.getElementById(popupId);
+		// const targetElem = popup.querySelector(`.${input}`);
+		const activeElement = popup.querySelector(`.${activeEl}`);
+		
+		popup.addEventListener('click', (e) => {
+			const target = e.target;
+			
+			if(target.closest(`.${input}`)){
+				activeElement.classList.add(`${activeEl}-active`);
+			};
+			if(target.closest(`.${autoCloseClassName}`)){
+				activeElement.classList.remove(`${activeEl}-active`);
+			}
+			// console.log(activeElement);
+			// console.log(target);
+			// console.log(target.closest(`.${activeEl}-active`));
+			// console.log(target.closest(".datepicker--cell-day"));
+		
+			else if(activeElement.matches(`.${activeEl}-active`) && target.matches('.popup')){
+				activeElement.classList.remove(`${activeEl}-active`);
+			}
+		})
+}
+
+selectDate('popupForm','datepicker-here','my-datepicker','datepicker--cell-day');
+
+const myForm = new Popup('formWrap', '.nav__btn','.closeButton', missed=false)
