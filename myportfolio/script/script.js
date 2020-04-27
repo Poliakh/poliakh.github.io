9e966866f393ce6edd3f44043d8205e8d3c86013
Roman <r.poliakh@gmail.com>
2020-04-27 15:21:06 +0300

    - changed for template Portfolio

--- myportfolio/script/script.js
+++ myportfolio/script/script.js
@@ -1,48 +1,79 @@
-// --Burger--
-(function () {
-	let burgerButton = document.querySelector('.burger');
-	let burgerLine = document.querySelector('.bline');
-	let navMenu = document.querySelector('.menu');
+"use strict";
 
-	document.querySelector('body').addEventListener('mouseup', menuHover);
-	function menuHover(event) {
+const burger = () => {
+	const wrapMenuNavigation = document.querySelector('.navbar');
+	document.querySelector('body').addEventListener('click', toggleMenu);
+
+	function toggleMenu(event) {
 		if (event.target.closest('.burger') || event.target.closest('.menu')) {
-			burgerButton.classList.toggle('burgerActive');
-			burgerLine.classList.toggle('burgerLineAnimate');
-			navMenu.classList.toggle('menuHover');
+			wrapMenuNavigation.classList.toggle('navbar-active');
 		}
 		if (!event.target.closest('.burger')) {
-			burgerButton.classList.remove('burgerActive');
-			burgerLine.classList.remove('burgerLineAnimate');
-			navMenu.classList.remove('menuHover');
+			wrapMenuNavigation.classList.remove('navbar-active');
 		}
 	}
-})()
-// --burger end--
+}
 
-function AjaxSelect(wrapElem, card) {
-	let myCards = document.createDocumentFragment();
-	let parentCards = document.querySelector(wrapElem);
-	let elemCard = parentCards.querySelector(card);
+//turn off animated navigation for items menu  if burger visible
+(function () {
+	const burger = document.querySelector(".burger")
+	if (getComputedStyle(burger).visibility == 'visible') {
+		const navItemList = document.querySelectorAll(".menu .wow");
+		navItemList.forEach(elem => {
+			elem.classList.remove('wow');
+		})
+	}
+})(); 
 
-	let showLoading = (on) => {
+burger();
+class CardListCreator {
+	constructor(wrapElem, card){
+		this.card = card;
+		this.myCards = document.createDocumentFragment();
+		this.parentCards = document.querySelector(wrapElem);
+		this.elemCard = this.parentCards.querySelector(card);
+		console.log('creator');
+		
+	};
+
+	_showLoading = (state = true) => {
 		let loader = document.querySelector('.loader');
-		(on) ? loader.classList.remove('loaderHide') : loader.classList.add('loaderHide')
-	}
+		(state) ? loader.classList.remove('loaderHide') : loader.classList.add('loaderHide');
+	};
+	
+	createLinkToGoogleSheet = (shareLink = '', listNumber = 1) => {
+		const idRegex = /\/d\/(.*)\//;
+		try {
+			const idSheet = shareLink.match(idRegex)[1];
+			return `https://spreadsheets.google.com/feeds/list/${idSheet}/${listNumber}/public/values?alt=json`
+		} catch (error) {
+			console.error('link for sheet must be "https:/*/d/*');
+		};
+	};
 
-	let checkStatus = (response) => {
-		if (response.ok) {
-			return response.json()
-		} else {
-			showLoading(false);
-			let error = new Error("Ошибка " + response.statusText);
-			error.response = response;
-			throw error;
+	createListIconStackTechnology = (obj, newCard) => {
+		const listStack = obj.stack;
+		const listIcon = newCard.querySelectorAll('[data-id-svg]');
+		//Mark the necessary SVG elements in the map tree
+		for (let i = 0; i < listStack.length; i++) {
+			for (let index = 0; index < listIcon.length; index++) {
+				if (listStack[i] == listIcon[index].dataset.idSvg) {
+					listIcon[index].dataset.idSvg = 'is';
+				}
+			}
 		}
-	}
+		const parent = newCard.querySelector('.card__stack');
+		//Remove unnecessary items
+		for (let index = 0; index < listIcon.length; index++) {
+			if (listIcon[index].dataset.idSvg != 'is') {
+				parent.removeChild(listIcon[index]);
+			}
+		}
+		return newCard;
+	};
 
-	let createCard = (obj) => {
-		let newCard = elemCard.cloneNode(true);
+	_createCard = (obj) => {
+		const newCard = this.elemCard.cloneNode(true);
 		newCard.hidden = false;
 		newCard.querySelector('.descript__page').href = obj.gitpage;
 		newCard.querySelector('.descript__hub').href = obj.github;
@@ -50,92 +81,60 @@ function AjaxSelect(wrapElem, card) {
 		newCard.querySelector('.descript__text').innerHTML = obj.descript;
 		newCard.style.backgroundImage = `url("${obj.preview}")`;
 		
-		(function createListStack() {
-			let listStack = obj.stack;
-			let listIcon = newCard.querySelectorAll('[data-id-svg]');
-			//Mark the necessary SVG elements in the map tree
-			for (let i = 0; i < listStack.length; i++) {
-				for (let index = 0; index < listIcon.length; index++) {
-					if (listStack[i] == listIcon[index].dataset.idSvg) {
-						listIcon[index].dataset.idSvg = 'is';
-					}
-				}
-			}
-			let parent = newCard.querySelector('.card__stack');
-			//Remove unnecessary items
-			for (let index = 0; index < listIcon.length; index++) {
-				if (listIcon[index].dataset.idSvg != 'is') {
-					parent.removeChild(listIcon[index]);
-				}
-			}
-		})()
+		this.createListIconStackTechnology(obj,newCard)
 
 		return newCard;
-	}
+	};
 
-	let parseJSON = (obj) => {
+	_getObject = async (response) => {
+		if (response.ok) {
+			const data = (await response.json()).feed.entry[0].gsx$myjson.$t;
+			return JSON.parse(data);
+		} else {
+			showLoading(false);
+			const error = new Error("Ошибка " + response.statusText);
+			error.response = response;
+			throw error;
+		};
+	};
+
+	_parseJSON = (obj) => {
 		for (let key in obj) {
-			myCards.appendChild(createCard(obj[key]));
-		}
-		showLoading(false);
-		parentCards.appendChild(myCards);
-	}
+			if(obj[key].display){
+				this.myCards.appendChild(this._createCard(obj[key]));
+			};
+	
+		};
+		this._showLoading(false);
+		this.parentCards.appendChild(this.myCards);
+	};
 
-	this.load = (url) => {
-		showLoading(true);
-		while (parentCards.querySelector(card)) {
-			parentCards.removeChild(parentCards.querySelector(card));
-		}
-		fetchAsync = async () => {
+	load = (url) => {
+		this._showLoading(true);
+		//remove all children
+		while (this.parentCards.querySelector(this.card)) {
+			this.parentCards.removeChild(this.parentCards.querySelector(this.card));
+		};
+
+		const link = this.createLinkToGoogleSheet(url);
 			try {
-				let response = await fetch(url);
-				let obj = await checkStatus(response)
-				await parseJSON(obj);
-				await addDelayAnimation('.cardWrap', '.card', 0.2, 0.5);
+				fetch(link)
+				.then((data) => {
+					return this._getObject(data)
+				})
+				.then((obj) => {
+					this._parseJSON(obj);
+					console.log('obj: ', obj);
+					addDelayAnimation('.cardWrap', '.card', 0.2, 0.5);
+				})
+				
 			} catch (error) {
-				showLoading(false);
-				alert('request failed', error)
-			}
-		}
-		fetchAsync();
-	}
-}
-
-let ajaxSelect = new AjaxSelect('.cardWrap', '.card')
-ajaxSelect.load('data.json');
-
-//-------------------------------------<--
-
-
-
-// look it https://gist.github.com/msmfsd/fca50ab095b795eb39739e8c4357a808
-// 
-//--wow--
-new WOW().init();
-
-function addDelayAnimation(parentCLass, targetClass, incremdelay, delay) {
-	let listIcon = document.querySelector(parentCLass).querySelectorAll(targetClass);
-	listIcon.forEach(element => {
-		delay += incremdelay;
-		element.getAttribute('data-wow-delay');
-		element.dataset.wowDelay = delay + 's';
-	});
-}
-addDelayAnimation('.social', '.fadeInUp', 0.2, 0);
-addDelayAnimation('.contactlist', '.fadeInRight', 0.2, 0.8);
-addDelayAnimation('.menu', '.wow', 0.2, 0.5);
-addDelayAnimation('.cardWrap', '.card', 0.2, 0.5);
-//turn off animated navigation for items menu  if burger visible
-(function () {
-	let burger = document.querySelector(".burger")
-	if (getComputedStyle(burger).visibility == 'visible') {
-		let navItemList = document.querySelectorAll(".menu .wow");
-		navItemList.forEach(elem => {
-			elem.classList.remove('wow');
-		})
-	}
-})();
-
+				this._showLoading(false);
+				console.error('request failed ', error);
+				//вывести текст ошибки
+			};
+	};
+};
 //Copy by click
 document.querySelector('body').addEventListener('click', function (event) {
 	copyText('.details__icon', '.details__cont');
@@ -163,3 +162,32 @@ function copyText(clickElem, classTargetCopyText) {
 	}
 }
 //+38(063)420-60-46
+const createLinkToGoogleSheet = (shareLink = '', listNumber = 1) => {
+	const idRegex = /\/d\/(.*)\//;
+	try {
+		const idSheet = shareLink.match(idRegex)[1];
+		return `https://spreadsheets.google.com/feeds/list/${idSheet}/${listNumber}/public/values?alt=json`
+	} catch (error) {
+		console.error('link for sheet must be "https:/*/d/*');
+	};
+};
+
+// const url = createLinkToGoogleSheet(urlOriginal);
+
+const createList = new CardListCreator('.cardWrap', '.card')
+createList.load('https://docs.google.com/spreadsheets/d/1ki0LZ0At_vauhtubYrQP64AvXBv7Li-qcHzFz9wR0fs/edit?usp=sharing')
+
+new WOW().init();
+
+function addDelayAnimation(parentCLass, targetClass, incrementdelay, delay) {
+	const listIcon = document.querySelector(parentCLass).querySelectorAll(targetClass);
+	listIcon.forEach(element => {
+		delay += incrementdelay;
+		element.getAttribute('data-wow-delay');
+		element.dataset.wowDelay = delay + 's';
+	});
+}
+addDelayAnimation('.social', '.fadeInUp', 0.2, 0);
+addDelayAnimation('.contactlist', '.fadeInRight', 0.2, 0.8);
+addDelayAnimation('.menu', '.wow', 0.2, 0.5);
+addDelayAnimation('.cardWrap', '.card', 0.2, 0.5);
