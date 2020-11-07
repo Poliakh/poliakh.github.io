ec4bdedc65d5e62fc694abc29ec1db2f462b39ef
Roman <r.poliakh@gmail.com>
2020-11-07 19:50:48 +0200

    - create gag

--- myportfolio/site/nvmed/script/video.js
+++
@@ -1,8 +0,0 @@
-function resize(){
-	let widthBrows = document.documentElement.clientWidth ;
-	let heightBrows = document.documentElement.clientHeight;
-	let heightVideo = Math.floor(widthBrows / 1.777778) ;
-	document.getElementById('widthVideo').style.height = heightVideo +'px';
-};
-resize();
-window.onresize = resize;
